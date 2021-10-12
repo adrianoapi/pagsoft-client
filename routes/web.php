fc06ea54ff7893546ada9d21660726f0da7caf95
adrianoapi <adrianoapi@hotmail.com>
2021-10-12 15:22:50 -0300

    create web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 'LoginController@index');
+Route::post('/login', 'LoginController@auth')->name('login.auth');
+
+Route::get('/auth', 'ClientController@auth');
+Route::get('/client', 'ClientController@index');
+Route::get('/collection', 'ClientController@getCollection');
