50950aa88b092db36ca21f3d1ca7887bdf1ea7b7
Adriano A Costa <adrianoapi@hotmail.com>
2022-08-01 11:53:55 -0300

    add condition delete

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -85,141 +85,137 @@
 
  
 
-    $(document).ready(function () {
-       
-       $.ajaxSetup({
-           headers: {
-           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-           }
-       });
-         
-       var calendar = $('#calendar').fullCalendar({
-                           editable: true,
-                           events: "{{route('event.index')}}",
-                           eventColor: "#3788d8",
-                           eventTextColor: "#fff",
-                           displayEventTime: false,
-                           editable: true,
-                           eventRender: function (event, element, view) {
-                               if (event.allDay === 'true') {
-                                       event.allDay = true;
-                               } else {
-                                       event.allDay = false;
-                               }
-                           },
-                           selectable: true,
-                           selectHelper: true,
-                           select: function (start, end, allDay) {
-                               var title = prompt('Event Title:');
-                               if (title) {
-                                   var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
-                                   var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
-                                   $.ajax({
-                                       url: "{{route('event.store')}}",
-                                       data: {
-                                           title: title,
-                                           start: start,
-                                           end: end,
-                                           location: "Rua Epaminondas",
-                                           all_day: false,
-                                           type: 'add',
-                                           _token: "{{ csrf_token() }}",
-                                       },
-                                       type: "POST",
-                                       success: function (data) {
-                                           console.log(data);
-                                           displayMessage("Event Created Successfully");
-         
-                                           calendar.fullCalendar('renderEvent',
-                                               {
-                                                   id: data.id,
-                                                   title: title,
-                                                   start: start,
-                                                   end: end,
-                                                   allDay: allDay
-                                               },true);
-         
-                                           calendar.fullCalendar('unselect');
-                                       }
-                                   });
-                               }
-                           },
-                           
-                           eventDrop: function (event, delta) {
-                               var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
-                               var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
-         
-                               $.ajax({
-                                   url: "{{route('event.update')}}",
-                                   data: {
-                                       title: event.title,
-                                       start: start,
-                                       end: end,
-                                       id: event.id,
-                                       type: 'update',
-                                       _token: "{{ csrf_token() }}"
-                                   },
-                                   type: "POST",
-                                   success: function (response) {
-                                       displayMessage("Event Updated Successfully");
-                                   }
-                               });
-                           },
-
-                           eventClick: function (event) {
-
-                            //$('#eventModal').modal('toggle');
-                            //$('#eventModal').modal('show');
-                            //$('#eventModal').modal('hide');
-                            //return false;
-
-                            
-                            $('#eventModal').modal('show');
-
+$(document).ready(function () {
+    
+    $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+        
+    var calendar = $('#calendar').fullCalendar({
+                        editable: true,
+                        events: "{{route('event.index')}}",
+                        eventColor: "#3788d8",
+                        eventTextColor: "#fff",
+                        displayEventTime: false,
+                        editable: true,
+                        eventRender: function (event, element, view) {
+                            if (event.allDay === 'true') {
+                                    event.allDay = true;
+                            } else {
+                                    event.allDay = false;
+                            }
+                        },
+                        selectable: true,
+                        selectHelper: true,
+                        select: function (start, end, allDay) {
+                            var title = prompt('Event Title:');
+                            if (title) {
+                                var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
+                                var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
+                                $.ajax({
+                                    url: "{{route('event.store')}}",
+                                    data: {
+                                        title: title,
+                                        start: start,
+                                        end: end,
+                                        location: "Rua Epaminondas",
+                                        all_day: false,
+                                        type: 'add',
+                                        _token: "{{ csrf_token() }}",
+                                    },
+                                    type: "POST",
+                                    success: function (data) {
+                                        console.log(data);
+                                        displayMessage("Event Created Successfully");
+        
+                                        calendar.fullCalendar('renderEvent',
+                                            {
+                                                id: data.id,
+                                                title: title,
+                                                start: start,
+                                                end: end,
+                                                allDay: allDay
+                                            },true);
+        
+                                        calendar.fullCalendar('unselect');
+                                    }
+                                });
+                            }
+                        },
+                        
+                        eventDrop: function (event, delta) {
+                            var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
+                            var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
+        
                             $.ajax({
-                                type: "GET",
-                                url: "{{route('event.show')}}",
+                                url: "{{route('event.update')}}",
                                 data: {
-                                        id: event.id,
-                                        _token: "{{ csrf_token() }}"
+                                    title: event.title,
+                                    start: start,
+                                    end: end,
+                                    id: event.id,
+                                    type: 'update',
+                                    _token: "{{ csrf_token() }}"
                                 },
+                                type: "POST",
                                 success: function (response) {
-                                    var data = JSON.parse(response);
-                                    console.log(response);
-
-                                    $("#eventModalLabel").html(data.title);
-                                    $("#tbEventColor").html(data.backgroundColor);
-                                    $("#tbEventEditable").html(data.editable);
-                                    $("#tbLocation").html(data.location);
-                                    $("#tbAll_day").html(data.all_day);
-                                    
+                                    displayMessage("Event Updated Successfully");
                                 }
                             });
+                        },
 
-                               /*var deleteMsg = confirm("Do you really want to delete?");
-                               if (deleteMsg) {
-                                   $.ajax({
-                                       type: "POST",
-                                       url: "{{route('event.delete')}}",
-                                       data: {
-                                               id: event.id,
-                                               type: 'delete',
-                                               _token: "{{ csrf_token() }}"
-                                       },
-                                       success: function (response) {
-                                           calendar.fullCalendar('removeEvents', event.id);
-                                           displayMessage("Event Deleted Successfully");
-                                       }
-                                   });
-                               }*/
-                           }
-        
-                       });
-        
-       });
-        
-       function displayMessage(message) {
-            demo.showNotification('top','right', message, 2)
-       } 
+                        eventClick: function (event) {
+
+                            var deleteMsg = confirm("Do you really want to delete?");
+                                if (deleteMsg) {
+                                    $.ajax({
+                                        type: "POST",
+                                        url: "{{route('event.delete')}}",
+                                        data: {
+                                                id: event.id,
+                                                type: 'delete',
+                                                _token: "{{ csrf_token() }}"
+                                        },
+                                        success: function (response) {
+                                            calendar.fullCalendar('removeEvents', event.id);
+                                            displayMessage("Event Deleted Successfully");
+                                        }
+                                    });
+                                }else
+                                {
+                                    $('#eventModal').modal('show');
+
+                                    $.ajax({
+                                        type: "GET",
+                                        url: "{{route('event.show')}}",
+                                        data: {
+                                                id: event.id,
+                                                _token: "{{ csrf_token() }}"
+                                        },
+                                        success: function (response) {
+                                            var data = JSON.parse(response);
+                                            console.log(response);
+
+                                            $("#eventModalLabel").html(data.title);
+                                            $("#tbEventColor").html(data.backgroundColor);
+                                            $("#tbEventEditable").html(data.editable);
+                                            $("#tbLocation").html(data.location);
+                                            $("#tbAll_day").html(data.all_day);
+                                            
+                                        }
+                                    });
+                                }
+
+                        }
+    
+    });
+    
+});
+
+function displayMessage(message) {
+    demo.showNotification('top','right', message, 2)
+} 
     </script>
 @endsection
